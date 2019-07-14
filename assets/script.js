3a71b5fffb30fe0fa590e1b1511cdd87c1cfbf5e
lunanie <lunanie@gmail.com>
2019-07-13 21:46:25 -0300

    validando campos

--- assets/script.js
+++ assets/script.js
@@ -93,3 +93,23 @@ botao.addEventListener("click", () => {
   });
   sectionCards.innerHTML = createItens(filtro);
 });
+
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Pega todos os formulários que nós queremos aplicar estilos de validação Bootstrap personalizados.
+    var forms = document.getElementsByClassName('needs-validation');
+    // Faz um loop neles e evita o envio
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
